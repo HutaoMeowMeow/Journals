23277f52b8bd9c5cb7bdf3674dccd7b8149c0d4c
Gian <hutao260132@gmail.com>
2026-07-31 08:52:45 +0100

    Your commit message here

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
@@ -18,24 +17,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-=======
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDf63WBb_Eho1LO4E6QYYO6rjNESrGAGvI",
-  authDomain: "journal-96fc3.firebaseapp.com",
-  projectId: "journal-96fc3",
-  storageBucket: "journal-96fc3.firebasestorage.app",
-  messagingSenderId: "82648709349",
-  appId: "1:82648709349:web:778e0a9d3ccc196db7bc0c"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
->>>>>>> 66128392099fbf510efa475ffb8f6bf08c7b1442
